a7e359f7eea9e22d54712a7e24803fcd78f1683a
priyanshu <priyanshuraja456@gmail.com>
2023-04-08 15:44:12 +0530

    chaneged some shit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,34 @@ import Header from './header';
 import Hero from './hero';
 import Login from './login';
 import Signup from './signup';
+import Root from './RootLayout';
+import Products from './Products';
+import About from './about';
+import { Route, createRoutesFromElements } from 'react-router-dom';
 function App(){
     return (
-     <div className=' p-4'>
-          <Login/>
-    </div>
+      createBrowserRouter(
+        createRoutesFromElements(
+          <Route path="/" element={<Root />}>
+            <Route path = "/" element={<Header/>}>
+              <Route index element={<Hero />} />
+              <Route path="products" element={<Products />} />
+              <Route path="about" element={<div><About/></div>} />
+            
+            
+            </Route>
+               
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path = "auth/google/home" element = {<div>google auth</div>}/>
+      
+          </Route>
+      
+        )
+      )
+     
+     
+     
     )
 }
 export default App
--- src/index.js
+++ src/index.js
@@ -15,26 +15,6 @@ import Root from "./components/RootLayout.js"
 import Products from "./components/Products.jsx"
 import About from "./components/about.jsx"
 const user = false;
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Root />}>
-      <Route path = "/" element={<Header/>}>
-        <Route index element={<Hero />} />
-        <Route path="products" element={<Products />} />
-        <Route path="about" element={<div><About/></div>} />
-      
-      
-      </Route>
-         
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<Signup />} />
-      <Route path = "auth/google/home" element = {<div>google auth</div>}/>
-
-    </Route>
-
-  )
-);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
